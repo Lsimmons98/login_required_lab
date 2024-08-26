848e2f90c7426c347bedca0925be77e376f439e7
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-26 10:23:40 -0400

    implement login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :secrets, only: [:show]
+
+  get 'show', to: 'secrets#show'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'products#index'
 end
